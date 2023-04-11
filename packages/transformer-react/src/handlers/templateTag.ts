091ecd79e20461c73810553d842767834feadb02
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-04-11 21:32:35 +0200

    fix(KazFileParser): make children optional in Tag schema

--- packages/transformer-react/src/handlers/templateTag.ts
+++ packages/transformer-react/src/handlers/templateTag.ts
@@ -2,6 +2,9 @@ import type { IHandler } from '../transformer-react'
 
 export const handleTemplateTag: IHandler<'templateTag'> = async (templateTag, { handle }) => {
   return `<${templateTag.tagName} `
-    + `${(await Promise.all(templateTag.attributes.map(attribute => handle(attribute)))).join(' ')}>`
-    + `>${(await Promise.all(templateTag.children.map(child => handle(child)))).join('\n')}</${templateTag.tagName}>`
+    + `${(await Promise.all(templateTag.attributes.map(attribute => handle(attribute)))).join(' ')}`
+    + `${(templateTag.children === undefined)
+      ? '/>'
+      : `>${(await Promise.all(templateTag.children.map(child => handle(child)))).join('\n')}</${templateTag.tagName}>`
+    }`
 }
