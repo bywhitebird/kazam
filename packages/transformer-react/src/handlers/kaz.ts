c2df0e0cdeaf5b3faf3ae10e9e28cb619d9dbec2
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-06-18 17:47:45 +0200

    fix(TransformerReact): write instructions in the correct order

--- packages/transformer-react/src/handlers/kaz.ts
+++ packages/transformer-react/src/handlers/kaz.ts
@@ -3,13 +3,11 @@ import * as path from 'node:path'
 import type { IHandler } from '../transformer-react'
 
 export const handleKaz: IHandler<'ast'> = async (kaz, { handle, componentMeta, addImport }) => {
-  addImport({ defaultImport: { name: 'React' }, path: 'react' })
+  addImport({ wildcardImport: { alias: 'React' }, path: 'react' })
 
   const importInstructions = kaz.instructions.filter(instruction => instruction.$type === 'ImportInstruction')
-  const watchInstructions = kaz.instructions.filter(instruction => instruction.$type === 'WatchInstruction')
-  const computedInstructions = kaz.instructions.filter(instruction => instruction.$type === 'ComputedInstruction')
-  const stateInstructions = kaz.instructions.filter(instruction => instruction.$type === 'StateInstruction')
   const propInstructions = kaz.instructions.filter(instruction => instruction.$type === 'PropInstruction')
+  const otherInstructions = kaz.instructions.filter(instruction => instruction.$type !== 'ImportInstruction' && instruction.$type !== 'PropInstruction')
 
   await Promise.all(importInstructions.map(instruction => handle(instruction)))
 
@@ -21,9 +19,7 @@ export const handleKaz: IHandler<'ast'> = async (kaz, { handle, componentMeta, a
 
   return `
     export const ${componentName} = (${props.length > 0 ? `{ ${propsDeclaration} }: { ${propsType} }` : ''}) => {
-      ${await Promise.all(stateInstructions.map(instruction => handle(instruction))).then(instructions => instructions.join('\n'))}
-      ${await Promise.all(computedInstructions.map(instruction => handle(instruction))).then(instructions => instructions.join('\n'))}
-      ${await Promise.all(watchInstructions.map(instruction => handle(instruction))).then(instructions => instructions.join('\n'))}
+      ${await Promise.all(otherInstructions.map(instruction => handle(instruction))).then(instructions => instructions.join('\n'))}
 
       return (<>
         ${await Promise.all(kaz.template.map(instruction => handle(instruction))).then(instructions => instructions.join('\n'))}
