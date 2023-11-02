b60c5266081c50e741985cf4b29e0e4e7396c32f
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-11-03 00:20:55 +0100

    fix(TransformerReact): remove duplicate imports

--- packages/transformer-react/src/transformer-react/transform/transform-service/functions/get-import-string.ts
+++ packages/transformer-react/src/transformer-react/transform/transform-service/functions/get-import-string.ts
@@ -2,8 +2,34 @@ import * as path from 'node:path'
 
 import { Effect, Ref } from 'effect'
 
+import type { Import } from './add-import'
 import { ImportStateService } from '../states/import-state'
 import { TransformService } from '../transform-service'
+
+const removeDuplicatedImports = (imports: Import[]) => {
+  return imports.filter((import_, index) => {
+    const firstIndex = imports.findIndex((import__) => {
+      if (import_.path !== import__.path)
+        return false
+
+      if (import_.type === 'defaultImport' && import__.type === 'defaultImport')
+        return import_.name === import__.name && import_.typeOnly === import__.typeOnly
+
+      if (import_.type === 'namedImport' && import__.type === 'namedImport')
+        return import_.name === import__.name && import_.alias === import__.alias && import_.typeOnly === import__.typeOnly
+
+      if (import_.type === 'namespaceImport' && import__.type === 'namespaceImport')
+        return import_.alias === import__.alias && import_.typeOnly === import__.typeOnly
+
+      if (import_.type === 'sideEffectImport' && import__.type === 'sideEffectImport')
+        return true
+
+      return false
+    })
+
+    return firstIndex === index
+  })
+}
 
 export const getImportString = () =>
   Effect.gen(function* (_) {
@@ -19,7 +45,7 @@ export const getImportString = () =>
     if (imports === undefined)
       return ''
 
-    return imports.map((import_) => {
+    return removeDuplicatedImports(imports).map((import_) => {
       // The following block will fix the import path if it is a relative path
       if (import_.path.startsWith('.')) {
         const absoluteImportedFilePath = path.resolve(
