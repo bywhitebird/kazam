c291d1afff7f16eb906a932d40c018eb98b16dd8
Arthur Fontaine <0arthur.fontaine@gmail.com>
2023-11-13 20:03:12 +0100

    refactor(Alakazam): remove welcome page

--- apps/alakazam/src/features/welcome/routes.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtRoutes } from "~/shared/utils/define-nuxt-routes";
-
-export default defineNuxtRoutes(__dirname, [
-  {
-    name: 'welcome-page',
-    path: '/',
-    file: './pages/welcome.vue',
-  },
-])
